6e7e39cdb02158a5b2e6b0d1ffd9f2501592d16b
Pranjalkumari <kumaripranjal694@gmail.com>
2026-01-02 12:20:27 +0530

    iinstall latest version of vite to allow tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChatPage from './pages/ChatPage';
 import AnalyticsPage from './pages/AnalyticsPage';
+import './App.css'
 
 function App() {
   return (
+
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ChatPage />} />
         <Route path="/admin" element={<AnalyticsPage />} />
+        
       </Routes>
     </BrowserRouter>
   );
